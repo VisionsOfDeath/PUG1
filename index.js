247ce81b1d061d1fe88f8530f878281c9ecf8f1c
VisionsOfDeath <revokher@hotmail.co.za>
2016-11-27 17:59:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -74,8 +74,9 @@ var heroList = [
 var listenTo = ""; // ID of user who has the bot's attention
 var pickVoiceChannel = ""; // voice channel object where players are picked from
 // accepted text channel id
-var goonGeneralChannel = "187021962357243904";
-
+var goonGeneralChannel = "252461827361996810";
+//252461827361996810
+//187021962357243904
 /************** Functions **************/
 // returns string of availible players in pool
 function listAvailiblePlayers() {
